7948c2b5b5a13c35c92fcffa0d8a251aba9fb68f
thamizh4yazhini <ts8454srmist.edu.in>
2026-03-02 21:18:15 +0530

    feat:implement UC5 in a better concise way than UC4

---
+++ UC5.java
@@ -0,0 +1,21 @@
+
+public class UC5 {
+
+    
+    public static void main(String[] args) {
+
+        String[] lines = {
+            String.join(" ", "  *** ", "  *** ", " ***** ", " ***** "),
+            String.join(" ", " ** **", " ** **", " ** **", " ** "),
+            String.join(" ", "** **", "** **", "** **", "** "),
+            String.join(" ", "** **", "** **", "***** ", " ***** "),
+            String.join(" ", "** **", "** **", "** ", "      **"),
+            String.join(" ", " ** **", " ** **", "** ", " ** **"),
+            String.join(" ", "  *** ", "  *** ", "** ", " ***** ")
+        };
+
+
+            System.out.println(line);
+        }
+    }
+}
