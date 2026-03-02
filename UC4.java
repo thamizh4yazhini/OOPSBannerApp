a3eb6bc4f2d00e9ad009c1095d3f098019bb47cf
thamizh4yazhini <ts8454srmist.edu.in>
2026-03-02 20:58:09 +0530

    feat: implement UC4 using String array and for-each loop

---
+++ UC4.java
@@ -0,0 +1,20 @@
+
+public class UC4{
+
+    public static void main(String[] args) {
+  
+        String[] bannerLines = {
+            String.join("  ", "  *** ", "   *** ", "   ***** ", "  ***** "),
+            String.join("  ", " ** ** ", " **  ** ", " ** ** ", " ** "),
+            String.join("  ", "**   **", "**   **", "  ** ** ", " ** "),
+            String.join("  ", "**   **", "**   **", "  ***** ", "  *****"),
+            String.join("  ", "**   **", "**   **", "  ** ", "        **"),
+            String.join("  ", " ** ** ", " **  ** ", " ** ", "   ** **"),
+            String.join("  ", "  *** ", "   *** ", "   ** ", "   **** ")
+        };
+
+
+            System.out.println(line);
+        }
+    }
+}
